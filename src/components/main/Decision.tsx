4cda2db0d82d6322d6896beda856ebca42b123f9
Sadman Hossain <josephitesadman56@gmail.com>
2024-07-06 15:40:16 +0600

    Ginimachine texts to Rini

--- src/components/main/Decision.tsx
+++ src/components/main/Decision.tsx
@@ -5,9 +5,9 @@ export default function Decision() {
           <div className="pr-16 w-[60%]">
             <h2 className="text-6xl md:text-5xl font-bold">Automated Decision-Making Platform</h2>
             <p className="py-8 text-md text-[#74788b]">
-            Say goodbye to antiquated spreadsheets and static data visualization! Embrace the power of AI and gain a glimpse into the future. GiniMachine algorithms not only simulate and compare, but they also ignite your strategic action plan.
+            Say goodbye to antiquated spreadsheets and static data visualization! Embrace the power of AI and gain a glimpse into the future. Rini algorithms not only simulate and compare, but they also ignite your strategic action plan.
             <br />
-No need to have ML engineers or large teams of data scientists on board. GiniMachine AI-powered decisioning platform can process terabytes of historical data. It builds, validates, and deploys predictive models in minutes, not days.
+No need to have ML engineers or large teams of data scientists on board. Rini AI-powered decisioning platform can process terabytes of historical data. It builds, validates, and deploys predictive models in minutes, not days.
             </p>
           </div>
         </div>
